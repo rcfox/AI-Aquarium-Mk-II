b22081f0dcd8e467e6ecb0e0710fecc4c0ef5236
Ryan Fox <foxryan@gmail.com>
2012-01-29 00:35:57 -0500

    Add a function to get the deepest sub-quadtree that contains the bounding box given. This let us save some recursions when making multiple searches in the same region.

--- tree/quadtree.scm
+++ tree/quadtree.scm
@@ -2,7 +2,7 @@
   #:use-module (srfi srfi-1)
   #:export (quadtree
 			make-quadtree
-			quadtree-insert! quadtree-remove! quadtree-search))
+			quadtree-insert! quadtree-remove! quadtree-search quadtree-get-region))
 
 (define quadtree (make-record-type 'quadtree '(position size children)))
 (define make-quadtree (lambda* (#:optional #:key (size 1) (pos '(0 . 0)))
@@ -47,6 +47,19 @@
 									 (append-map (lambda (t) (do-at-position t pos proc)) (children tree))))
 							   '()))))
 
+(define quadtree-get-region (lambda (tree top-left bottom-right)
+							  (let ((next (call/cc (lambda (return)
+													 (for-each (lambda (child)
+																 (let* ((tl (position child))
+																		(br (p+n tl (size child))))
+																   (if (and (between? tl br top-left) (between? tl br bottom-right))
+																	   (return child))))
+															   (children tree))
+													 '()))))
+								(if (null? next)
+									tree
+									(quadtree-get-region next top-left bottom-right)))))
+
 (define quadtree-insert! (lambda (tree pos item)
 				  (do-at-position tree pos (lambda (tree)
 											 (set-children! tree (cons item (children tree)))
@@ -60,3 +73,12 @@
 (define quadtree-search (lambda (tree pos)
 				 (do-at-position tree pos (lambda (tree)
 											(children tree)))))
+(define test (lambda ()
+			   (let ((tree (make-quadtree #:size 32)))
+				 (quadtree-insert! tree '(0 . 0) 'a)
+				 (quadtree-insert! tree '(1 . 0) 'b)
+				 (quadtree-insert! tree '(1 . 1) 'c)
+				 (quadtree-insert! tree '(3 . 8) 'd)
+				 (quadtree-insert! tree '(13 . 5) 'e)
+				 (quadtree-insert! tree '(7 . 7) 'f)
+				 tree)))
